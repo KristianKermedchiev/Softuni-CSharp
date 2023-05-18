53eeaaefaa0c8f05ea1470adae8ab8c1f093befc
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-05-18 22:52:42 +0300

    First Lesson

---
+++ 01. First Steps in C#/Lab/02. Numbers 1 to 10.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Numbers_1_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);     
+            }
+        }
+    }
+}
