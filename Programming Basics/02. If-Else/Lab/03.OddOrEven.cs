82a9be5f7390b13d53a17c6aaea7f95fae72e658
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-06-22 21:29:05 +0300

    Lab problems If-Else

---
+++ Programming Basics/02. If-Else/Lab/03.OddOrEven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.OddOrEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
