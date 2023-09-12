14b13d1d9cfeda84cc809ec0a89fe713f93f2b6b
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-09-13 00:50:49 +0300

    Exercise 1 files

---
+++ C# Advanced/01. Stacks and Queues/Lab/Lab Exercises/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿string input = Console.ReadLine();
+
+Stack<char> stack = new Stack<char>();
+
+foreach (var character in input)
+{
+    stack.Push(character);
+}
+
+while (stack.Count > 0)
+{
+    Console.Write(stack.Pop());
+}
