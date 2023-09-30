50fcd2b5e7b13f3d2f3920ea7b414dc4cc512bb5
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-09-30 12:42:22 +0300

    Exercise 1 and 2

--- C# Advanced/02. Multidimensional Arrays/Lab/1. Sum Matrix Elements/Program.cs
+++ C# Advanced/02. Multidimensional Arrays/Lab/1. Sum Matrix Elements/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int rows = (sizes[0]);
+int cols = (sizes[1]);
+
+int[, ] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] rowValues = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = rowValues[col];
+    }
+}
+
+int sum = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col <cols; col++)
+    {
+        sum += matrix[row, col];
+    }
+}
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
---
+++ C# Advanced/02. Multidimensional Arrays/Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int rows = (sizes[0]);
+int cols = (sizes[1]);
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    
+    
+    int[] rowValues = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = rowValues[col];
+    }
+}
+
+
+
+for  (int col = 0; col < cols; col++)
+{
+    int sum = 0;
+
+    for (int row = 0; row < rows; row++)
+    {
+        sum += matrix[row, col];
+    }
+    Console.WriteLine(sum);
+}
