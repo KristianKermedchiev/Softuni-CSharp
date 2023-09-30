523928f17bef08e44def8d927e2998c5d7913bd3
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-09-30 14:10:05 +0300

    Exercises 3-7 added

---
+++ C# Advanced/02. Multidimensional Arrays/Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int sizes = int.Parse(Console.ReadLine());
+
+int rows = (sizes);
+int cols = (sizes);
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+
+    int[] rowValues = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = rowValues[col];
+    }
+}
+
+
+int sum = 0;
+
+for (int i = 0; i <matrix.GetLength(0); i++)
+{
+    sum += matrix[i,i];
+}
+
+Console.WriteLine(sum);
