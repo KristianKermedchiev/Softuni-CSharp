14bd4316d00b280c59942ed6d954d333b5255ff7
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-02 12:04:40 +0300

    Exercise 5

--- C# Advanced/03. Sets and Dictionaries Advanced/Lab/4. Product Shop/Program.cs
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/4. Product Shop/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string command = Console.ReadLine();
+
+
+Dictionary<string, Dictionary<string, double>> stores = new Dictionary<string, Dictionary<string, double>>();
+
+while  (command != "Revision")
+{
+
+    string[] partsCommand = command.Split(", ");
+    string shop = partsCommand[0];
+    string product = partsCommand[1];
+    double price = double.Parse(partsCommand[2]);
+
+    if (!stores.ContainsKey(shop)){
+        stores.Add(shop, new Dictionary<string, double>());
+    }
+        stores[shop].Add(product, price);
+
+    command = Console.ReadLine();
+}
+
+
+foreach (var (store,products) in stores.OrderBy(s => s.Key))
+{
+    Console.WriteLine($"{store}->");
+
+    foreach(var (product,price) in products)
+    {
+        Console.WriteLine($"Product: {product}, Price: {price}");
+    }
+}
---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/5. Cities by Continent and Country/Program.cs
@@ -0,0 +1,35 @@
+﻿int cities = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, List<string>>> places = new Dictionary<string, Dictionary<string, List<string>>>();
+
+for (int i = 0; i < cities; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    string continent = input[0];
+    string country = input[1];
+    string city = input[2];
+
+    if (!places.ContainsKey(continent))
+    {
+        places.Add(continent, new Dictionary<string, List<string>>());
+    }
+
+    if (!places[continent].ContainsKey(country)) 
+    {
+        places[continent].Add(country, new List<string>());
+    }
+
+    places[continent][country].Add(city);
+}
+
+
+foreach (var (continent,countries) in places)
+{
+    Console.WriteLine($"{continent}:");
+
+    foreach (var (country, city) in countries)
+    {
+        Console.WriteLine($"{country} -> {string.Join(", ", city)}");
+    }
+}
