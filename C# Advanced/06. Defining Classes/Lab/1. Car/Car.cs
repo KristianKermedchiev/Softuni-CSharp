b20ea57a14be22cd802956a02974300d265ec61a
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-11 19:54:25 +0300

    Lab problems 1-3

---
+++ C# Advanced/06. Defining Classes/Lab/1. Car/Car.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Text;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+           
+        }
+
+        public Car(string make, string model, int year): this ()
+        {
+            Model = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (distance * fuelConsumption > fuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {this.make}");
+            result.AppendLine($"Model: {this.model}");
+            result.AppendLine($"Year: {this.year}");
+            result.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Lab/1. Car/StartUp .cs
@@ -0,0 +1,21 @@
+﻿
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 2006;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
