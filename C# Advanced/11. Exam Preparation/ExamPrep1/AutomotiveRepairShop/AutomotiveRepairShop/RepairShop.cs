c4e865cf9b906aadc0e9953b0ec62d0acdd2c333
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-15 13:25:28 +0300

    Exam prep 3

---
+++ C# Advanced/11. Exam Preparation/ExamPrep1/AutomotiveRepairShop/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; private set; }
+
+        public List<Vehicle> Vehicles { get; private set;}
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Vehicles.Count < Capacity)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin) => Vehicles.Remove(Vehicles.FirstOrDefault(v => v.VIN == vin));
+
+        public int GetCount() => Vehicles.Count;
+
+        public Vehicle GetLowestMileage() => Vehicles.MinBy(v => v.Mileage);
+
+        public string Report()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine("Vehicles in the preparatory:");
+
+            foreach( var vehicle in Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
