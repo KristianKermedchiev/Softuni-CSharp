cc98bffb846d8b62ccb6d5925069831bbea4caee
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-20 15:18:24 +0300

    Exam prep problems

---
+++ C# Advanced/11. Exam Preparation/ExamPrep16OCT2023/02.DeliveryBoy/Program.cs
@@ -0,0 +1,189 @@
+﻿int[] dimensions = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+char[,] field = new char[dimensions[0], dimensions[1]];
+
+int boyRow = 0;
+int boyCol = 0;
+
+int boyInitialRow = 0;
+int boyInitialCol = 0;
+
+for (int row = 0; row < field.GetLength(0); row++)
+{
+    string newRow = Console.ReadLine();
+
+    for (int col = 0; col < field.GetLength(1); col++)
+    {
+        field[row, col] = newRow[col];
+
+        if (field[row, col] == 'B')
+        {
+            boyRow = row;
+            boyCol = col;
+
+            boyInitialRow = row;
+            boyInitialCol = col;
+        }
+    }
+}
+
+bool isOutsideFiled = false;
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "left")
+    {
+        if(boyCol == 0)
+        {
+            if (field[boyRow, boyCol] == '-')
+            {
+                field[boyRow, boyCol] = '.';
+            }
+
+            isOutsideFiled = true;
+
+            Console.WriteLine("The delivery is late. Order is canceled.");
+
+            break;
+        }
+
+        if (field[boyRow, boyCol - 1] == '*')
+        {
+            continue;
+        }
+
+        if (field[boyRow, boyCol] != 'R')
+        {
+            field[boyRow, boyCol] = '.';
+        }
+
+        boyCol--;
+    }
+    else if (command == "right")
+    {
+        if (boyCol == field.GetLength(1) - 1)
+        {
+            if (field[boyRow, boyCol] == '-')
+            {
+                field[boyRow, boyCol] = '.';
+            }
+
+            isOutsideFiled = true;
+
+            Console.WriteLine("The delivery is late. Order is canceled.");
+
+            break;
+        }
+
+        if (field[boyRow, boyCol + 1] == '*')
+        {
+            continue;
+        }
+
+        if (field[boyRow, boyCol] != 'R')
+        {
+            field[boyRow, boyCol] = '.';
+        }
+
+        boyCol++;
+    }
+    else if (command == "up")
+    {
+        if (boyRow == 0)
+        {
+            if (field[boyRow, boyCol] == '-')
+            {
+                field[boyRow, boyCol] = '.';
+            }
+
+            isOutsideFiled = true;
+
+            Console.WriteLine("The delivery is late. Order is canceled.");
+
+            break;
+        }
+
+        if (field[boyRow - 1, boyCol] == '*')
+        {
+            continue;
+        }
+
+        if (field[boyRow, boyCol] != 'R')
+        {
+            field[boyRow, boyCol] = '.';
+        }
+
+        boyRow--;
+    }
+    else if (command == "down")
+    {
+        if (boyRow == field.GetLength(0) - 1)
+        {
+            if (field[boyRow, boyCol] == '-')
+            {
+                field[boyRow, boyCol] = '.';
+            }
+
+            isOutsideFiled = true;
+
+            Console.WriteLine("The delivery is late. Order is canceled.");
+
+            break;
+        }
+
+        if (field[boyRow + 1, boyCol] == '*')
+        {
+            continue;
+        }
+
+        if (field[boyRow, boyCol] != 'R')
+        {
+            field[boyRow, boyCol] = '.';
+        }
+
+        boyRow++;
+    }
+
+    if (field[boyRow, boyCol] == 'P')
+    {
+        Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
+        field[boyRow, boyCol] = 'R';
+
+        continue;
+    }
+
+    if (field[boyRow, boyCol] == 'A')
+    {
+        Console.WriteLine("Pizza is delivered on time! Next order...");
+
+        field[boyRow, boyCol] = 'P';
+
+        break;
+    }
+}
+
+if (isOutsideFiled)
+{
+    field[boyInitialRow, boyInitialCol] = ' ';
+}
+else
+{
+    field[boyInitialRow, boyInitialCol] = 'B';
+
+}
+
+
+for (int row = 0; row < field.GetLength(0); row++)
+{
+    for (int col = 0; col < field.GetLength(1); col++)
+    {
+        Console.Write(field[row, col]);
+    }
+
+    Console.WriteLine();
+}
---
+++ C# Advanced/11. Exam Preparation/ExamPrep16OCT2023/VendingSystem/VendingMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            ButtonCapacity = buttonCapacity;
+            Drinks = new List<Drink>();
+        }
+
+        public int ButtonCapacity { get; set; }
+        public List<Drink> Drinks { get; set; }
+        public int GetCount => Drinks.Count;
+
+        public void AddDrink(Drink drink)
+        {
+            if (GetCount < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name) => Drinks.Remove(Drinks.FirstOrDefault(x => x.Name == name));
+
+        public Drink GetLongest() => Drinks.MaxBy(d => d.Volume);
+
+        public Drink GetCheapest() => Drinks.MinBy(d => d.Price);
+
+        public string BuyDrink(string name) => Drinks.FirstOrDefault(d => d.Name == name).ToString();
+
+        public string Report()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("Drinks available:");
+
+            Drinks.ForEach(x => sb.AppendLine(x.ToString()));
+
+            return sb.ToString().Trim();
+        }
+    }
+}
