89155b89594b2ff4b71b0f082536c17bbde958ee
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-31 11:14:34 +0200

    Exercise 4 100/100

---
+++ C# OOP/01. Inheritance/Lab/4.RandomList/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+       public static void Main()
+        {
+            RandomList list  = new RandomList();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
---
+++ C# OOP/01. Inheritance/Lab/4.RandomList/RandomList.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+        public string RandomString()
+        {
+            return this[random.Next(0, this.Count)];
+        }
+    }
+}
