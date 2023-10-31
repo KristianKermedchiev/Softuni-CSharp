90639fe00c934236d8eec1d648724c1608b65076
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-10-31 13:44:23 +0200

    Exercise 1 100/100

---
+++ C# OOP/01. Inheritance/Exercises/Person/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace Person
+{
+    internal class Person
+    {
+        private string name { get; set; }
+        private int age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
