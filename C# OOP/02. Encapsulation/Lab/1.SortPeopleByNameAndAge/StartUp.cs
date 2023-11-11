c8ce9f844c2b7c0185ed6c43b01b06c2f546c70e
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-11-11 21:39:46 +0200

    Encapsulation Lab - 1

---
+++ C# OOP/02. Encapsulation/Lab/1.SortPeopleByNameAndAge/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+
+            int age = int.Parse(personInfo[2]);
+
+            Person person = new Person(personInfo[0], personInfo[1], age);
+
+            people.Add(person);
+        }
+
+        foreach (var person in people.OrderBy(x => x.FirstName).ThenBy(x => x.Age))
+        {
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
