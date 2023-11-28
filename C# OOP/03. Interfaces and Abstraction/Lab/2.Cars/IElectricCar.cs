3f543d00e6a64f4a2fcb660d83421f10c0f00fb3
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-11-28 17:25:24 +0200

    Lab exercises 100/100

---
+++ C# OOP/03. Interfaces and Abstraction/Lab/2.Cars/IElectricCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Cars
+{
+    public interface IElectricCar
+    {
+        public int Battery { get; set; }
+
+
+    }
+}
