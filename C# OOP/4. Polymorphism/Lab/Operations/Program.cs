58f3fd410d73684afb99a9a61651efc7b860ac09
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-12-01 00:51:12 +0200

    Lab problem 1

---
+++ C# OOP/4. Polymorphism/Lab/Operations/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            Console.WriteLine(mathOperations.Add(5, 5));
+            Console.WriteLine(mathOperations.Add(5m, 5m, 6m));
+            Console.WriteLine(mathOperations.Add(5d, 5d, 7d));
+
+        }
+    }
+}
